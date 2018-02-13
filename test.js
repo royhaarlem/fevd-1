6e6c9bd46b4481e46260bd838544b4412478544e
annedegroot <annedegroot95@hotmail.com>
2018-02-13 10:31:57 +0100

    Versie 3
    Default op blauw. Switchen naar de verschillende kleuren vooruit.
    Achteruit lukt niet.

--- test.js
+++ test.js
@@ -1,16 +1,40 @@
 var paars = document.querySelector('.purple');
 
-var blauweplaatjes = document.querySelector('.blue-images');
-
 var paarseplaatjes = document.querySelector('.purple-images');
 
+var blauweplaatjes = document.querySelector('.blue-images');
 
-var paarseknop = function(event) {
-    blauweplaatjes.classList.toggle('paarseknop')
-    paarseplaatjes.classList.toggle('paarseknop')
+var groeneplaatjes = document.querySelector('.green-images');
+
+var rodeplaatjes = document.querySelector('.red-images')
+
+var paarseknop = function() {
+    paarseplaatjes.classList.toggle('paarseknop');
+    blauweplaatjes.classList.toggle('paarseknop');
+    groeneplaatjes.classList.toggle('paarseknop');
 }
 
 paars.addEventListener('click', paarseknop);
 
+var groen =  document.querySelector('.green');
 
+
+
+var groeneknop = function() {
+    groeneplaatjes.classList.toggle('groeneknop');
+    paarseplaatjes.classList.toggle('groeneknop')
+
+}
+
+groen.addEventListener('click', groeneknop);
+
+
+var rood = document.querySelector('.red');
+
+var rodeknop = function() {
+    groeneplaatjes.classList.toggle('rodeknop');
+    rodeplaatjes.classList.toggle('rodeknop');
+}
+
+rood.addEventListener('click', rodeknop);
 
